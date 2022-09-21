dbb91a034eac255568058b9c369acd185e95f44f
lekealex <106382948+lekealex@users.noreply.github.com>
2022-09-21 09:16:19 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  import axios from "axios";
+    export default {
+      data() {
+        return {
+          cep: "",
+          endereço: {},
+        };
+      },
+      methods: {
+        async buscar() {
+         const url = 'https://www.viacep.com.br/ws/$(this.cep)/json';
+         const res = await axios.get(url); 
+         this.endereço = res.data;
+        }
+      },
+    };
+  </script>
+<template>
+    <form @submit.prevent="buscar">
+      <input type="text" v-model="cep" placeholder="CEP:">
+      <button type="submit">Procurar</button>
+    </form>
+    <div class="end">
+      <p>Rua: {{endereço.logradouro}}</p>
+      <p>Número: {{endereço.numero}}</p>
+      <p>Bairro: {{endereço.bairro}}</p>
+      <p>Rua: {{endereço}}</p>
+      <p>Rua: {{endereço.lagradouro}}</p>
+    </div>
+</template>
