fbddb668c7c5c899ab8287cfc7e5a89ddfdbb7ec
lekealex <106382948+lekealex@users.noreply.github.com>
2022-09-22 14:07:34 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CepSearch
+# cepformulario
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,54 @@
 <script>
-  import axios from "axios";
-    export default {
-      data() {
-        return {
-          cep: "",
-          endereço: {},
-        };
-      },
-      methods: {
-        async buscar() {
-         const url = 'https://www.viacep.com.br/ws/$(this.cep)/json';
-         const res = await axios.get(url); 
-         this.endereço = res.data;
-        }
-      },
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      endereco: {},
     };
-  </script>
+  },
+  methods: {
+    async buscar() {
+      const url = `https:/viacep.com.br/ws/${this.cep}/json`;
+      const res = await axios.get(url);
+      this.endereco = res.data;
+    },
+  },
+};
+</script>
+
 <template>
+  <div class="container">
+    <h1>Busca CEP</h1>
     <form @submit.prevent="buscar">
-      <input type="text" v-model="cep" placeholder="CEP:">
-      <button type="submit">Procurar</button>
+      <input type="text" v-model="cep" placeholder="CEP" />
+      <button type="submit">Buscar</button>
     </form>
-    <div class="end">
-      <p>Rua: {{endereço.logradouro}}</p>
-      <p>Número: {{endereço.numero}}</p>
-      <p>Bairro: {{endereço.bairro}}</p>
-      <p>Rua: {{endereço}}</p>
-      <p>Rua: {{endereço.lagradouro}}</p>
-    </div>
+    <form class="end">
+      <label for="rua">Rua</label>
+      <input type="text" v-model="endereco.logradouro" />
+      <label for="rua">Bairro</label>
+      <input type="text" v-model="endereco.bairro" />
+      <label for="rua">DDD</label>
+      <input type="text" v-model="endereco.ddd" />
+      <label for="rua">Cidade</label>
+      <input type="text" v-model="endereco.localidade" />
+      <label for="rua">UF</label>
+      <input type="text" v-model="endereco.uf" />
+      <label for="rua">IBGE</label>
+      <input type="text" v-model="endereco.ibge" />
+    </form>
+  </div>
 </template>
+
+<style>
+.container {
+  background-color: #2d4350;
+  padding: 28px;
+}
+
+.end {
+  display: flex;
+  flex-direction: column;
+}
+</style>
